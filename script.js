1ff2fad91e4c305ffc6ecd176c4ad60e4b694581
Sanvi1317 <pbsanvi17@gmail.com>
2024-11-06 15:43:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,80 @@
+const cells = document.querySelectorAll('[data-cell]');
+const statusText = document.querySelector('.status');
+const restartButton = document.getElementById('restartButton');
+const X_CLASS = 'X';
+const O_CLASS = 'O';
+let circleTurn;
+
+const WINNING_COMBINATIONS = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+
+startGame();
+
+restartButton.addEventListener('click', startGame);
+
+function startGame() {
+    circleTurn = false;
+    cells.forEach(cell => {
+        cell.classList.remove(X_CLASS);
+        cell.classList.remove(O_CLASS);
+        cell.removeEventListener('click', handleClick);
+        cell.addEventListener('click', handleClick, { once: true });
+    });
+    statusText.textContent = "X's Turn";
+}
+
+function handleClick(e) {
+    const cell = e.target;
+    const currentClass = circleTurn ? O_CLASS : X_CLASS;
+    placeMark(cell, currentClass);
+    if (checkWin(currentClass)) {
+        endGame(false);
+    } else if (isDraw()) {
+        endGame(true);
+    } else {
+        swapTurns();
+        updateStatusText();
+    }
+}
+
+function placeMark(cell, currentClass) {
+    cell.classList.add(currentClass);
+}
+
+function swapTurns() {
+    circleTurn = !circleTurn;
+}
+
+function updateStatusText() {
+    statusText.textContent = `${circleTurn ? "O's" : "X's"} Turn`;
+}
+
+function checkWin(currentClass) {
+    return WINNING_COMBINATIONS.some(combination => {
+        return combination.every(index => {
+            return cells[index].classList.contains(currentClass);
+        });
+    });
+}
+
+function isDraw() {
+    return [...cells].every(cell => {
+        return cell.classList.contains(X_CLASS) || cell.classList.contains(O_CLASS);
+    });
+}
+
+function endGame(draw) {
+    if (draw) {
+        statusText.textContent = "Draw!";
+    } else {
+        statusText.textContent = `${circleTurn ? "O" : "X"} Wins!`;
+    }
+}
